44230c0c677dd2327e31f6d1061a24fc04e26a9b
HermesBoots <701@holbertonschool.com>
2019-09-09 18:28:51 +0000

    Add task 5: API endpoint to count data models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,23 @@
 
 
 from api.v1.views import app_views
+import json
+import models
+import models.base_model
 
 
 @app_views.route('/status')
 def api_status():
     """Report that the API is available"""
     return '{"status": "OK"}'
+
+
+@app_views.route('/stats')
+def api_countModels():
+    """Show the counts of each model type in storage"""
+    counts = {
+        name.lower(): models.storage.count(name)
+        for name, cls in models.classes.items()
+        if issubclass(cls, models.base_model.Base)
+    }
+    return json.dumps(counts)
