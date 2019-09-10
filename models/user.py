1b7413a9bd71a0446a2d7fbd7a7f3952a0eb0a4b
HermesBoots <701@holbertonschool.com>
2019-09-10 15:07:54 +0000

    Add task 15: always hash user passwords

--- models/user.py
+++ models/user.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 """ holds class User"""
+
+
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -13,17 +16,23 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        password = Column('password', String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """ensure that passwords are hashed"""
+        if name == 'password':
+            value = hashlib.md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
