c4322fe8a8b3f69354c8a727a7012f0b64472e2f
Nate Petersen <nate.petersen@me.com>
2019-09-10 20:10:52 +0000

    add delete method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -17,24 +17,28 @@ def get_state():
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state_id(state_id):
     """ retreive single state matching ID and return in JSON """
-    ret =  json.dumps(models.storage.get('State', state_id))
-    if get is None:
+    ret =  json.dumps(models.storage.get('State', state_id).to_dict())
+    if ret is None:
         abort(404)
     return ret
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
     """ delete state matching given ID. """
-
+    temp = storage.get('State', state_id)
+    temp.delete()
+    return '{}'
+    #storage.get and obj.delete()
+    #status 200 default (look into response object)
 
 @app_views.route('/states', methods=['POST'])
 def create_state():
     """ creates a new state object. """
-
+    #pass dict as kwargs to init method and save.
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update(state_id):
     """ update specific state object with new information. """
-
+    #return Response('Not a JSON', 400)
 
 x = request.get_json(silent=True)) #returns body of request as JSON-- if this is none, abort 400
