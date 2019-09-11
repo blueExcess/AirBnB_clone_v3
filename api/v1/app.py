4225bba5149dc7edfb54268657560ae985a9bea0
HermesBoots <701@holbertonschool.com>
2019-09-11 19:33:09 +0000

    Add task 13: send access control header when using API

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@
 
 from api.v1.views import app_views
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, origins='0.0.0.0', resources='/*')
 app.register_blueprint(app_views)
 
 
