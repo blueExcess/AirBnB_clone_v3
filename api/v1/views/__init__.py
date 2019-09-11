8d2fd614ba47655def17848c5bccd54cdb41bfa0
HermesBoots <701@holbertonschool.com>
2019-09-11 15:12:29 +0000

    Add task 16: search for places

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,4 @@ app_views = Blueprint('api', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
+from api.v1.views.places import *
---
+++ api/v1/views/places.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module for displaying and searching through places for rent"""
+
+
+from api.v1.views import app_views
+import flask
+import json
+import models
+
+
+@app_views.route('/places_search', methods=('POST',))
+def api_searchPlaces():
+    """Search through places"""
+    body = flask.request.get_json(silent=True)
+    if body is None:
+        flask.abort(400)
+    places = models.storage.all('Place').values()
+    if 'states' in body and len(body['states']) > 0:
+        places = [
+            place for place in places
+            if place.city.state.id in body['states']
+        ]
+    if 'cities' in body and len(body['cities']) > 0:
+        places = [
+            place for place in places
+            if place.city.id in body['cities']
+        ]
+    if 'amenities' in body and len(body['amenities']) > 0:
+        amenities = set(body['amenities'])
+        places = [
+            place for place in places
+            if amenities - set(am.id for am in place.amenities) == set()
+        ]
+    print(places[0].to_dict())
+    return '[' + ','.join(
+        json.dumps(place.to_dict())
+        for place in places
+    ) + ']'
