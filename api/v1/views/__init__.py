a7f5747149504f53c16c47abfb674060153124d3
HermesBoots <701@holbertonschool.com>
2019-09-12 17:39:18 +0000

    Update reference to places_amenities file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,7 +9,7 @@ app_views = Blueprint('api', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
-from api.v1.views.place_amenities import *
+from api.v1.views.places_amenities import *
 from api.v1.views.places import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
