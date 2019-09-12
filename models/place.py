cc05527d942fa15c488c1cca4164f9353198e991
HermesBoots <701@holbertonschool.com>
2019-09-12 03:44:39 +0000

    Rewrite Place.amenities to work when using FileStorage

--- models/place.py
+++ models/place.py
@@ -72,10 +72,8 @@ class Place(BaseModel, Base):
         @property
         def amenities(self):
             """getter attribute returns the list of Amenity instances"""
-            from models.amenity import Amenity
-            amenity_list = []
-            all_amenities = models.storage.all(Amenity)
-            for amenity in all_amenities.values():
-                if amenity.place_id == self.id:
-                    amenity_list.append(amenity)
-            return amenity_list
+            ret = [
+                amenity for amenity in models.storage.all('Amenity').values()
+                if amenity.id in self.amenity_ids
+            ]
+            return ret
