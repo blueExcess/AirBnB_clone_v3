e5b7815972e7e754e8fdc89bed6b69644dd02aed
Nate Petersen <nate.petersen@me.com>
2019-09-12 19:58:48 +0000

    also import make_response - scrub

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,7 +2,7 @@
 """ amenities view api commands/routes """
 
 from models.base_model import BaseModel
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, make_response, request
 from api.v1.views import app_views
 import models
 
